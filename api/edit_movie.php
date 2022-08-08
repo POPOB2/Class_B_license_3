1e21ab661a288a94b85adc1e8c290affd876c0c7
PB2 <nft192891@gmail.com>
2022-08-08 16:09:47 +0800

    完成院線片編輯電影功能

---
+++ api/edit_movie.php
@@ -0,0 +1,29 @@
+<!-- 8/8-15:25 ~ 16:00 -->
+<?php
+include_once "../base.php";
+$row=$Movie->find($_POST['id']);
+// 若使用isset會執行, 但不會成功
+if(!empty($_FILES['trailer']['tmp_name'])){ 
+    $_POST['trailer']=$_FILES['trailer']['name'];
+    move_uploaded_file($_FILES['trailer']['tmp_name'],'../upload/'.$_FILES['trailer']['name']);
+}else{
+    $_POST['trailer']=$row['trailer'];
+}
+if(!empty($_FILES['poster']['tmp_name'])){
+    $_POST['poster']=$_FILES['poster']['name'];
+    move_uploaded_file($_FILES['poster']['tmp_name'],'../upload/'.$_FILES['poster']['name']);
+}else{
+    $_POST['poster']=$row['poster'];
+}
+
+$_POST['ondate']=$_POST['year']."-".$_POST['month']."-".$_POST['day']; 
+unset($_POST['year'],$_POST['month'],$_POST['day']); 
+$_POST['sh']=$row['sh']; 
+$_POST['rank']=$row['rank'];
+
+// dd($row);
+// dd($_POST);
+$Movie->save($_POST);
+
+to("../back.php?do=movie");
+?>
