73277807520c8d9e45fb84f6f02a35323293afee
PB2 <nft192891@gmail.com>
2022-08-08 13:03:28 +0800

    製作新增院線片api功能

--- api/add_movie.php
+++ api/add_movie.php
@@ -1,26 +1,27 @@
 <!-- 8/8-11:25 ~ 11:55 -->
 <?php
 include_once "../base.php";
-
-if(isset($_FILES['trailer']['tmp_name'])){
+// $_FILES == 用POST上傳陣列
+if(isset($_FILES['trailer']['tmp_name'])){ // 用$_FILES上傳的陣列['']['']是否存在
     $_POST['trailer']=$_FILES['trailer']['name'];
-    move_uploaded_file($_FILES['trailer']['tmp_name'],'../upload'.$_FILES['trailer']['tmp_name']);
+    move_uploaded_file($_FILES['trailer']['tmp_name'],'../upload/'.$_FILES['trailer']['name']);
 }
 if(isset($_FILES['poster']['tmp_name'])){
     $_POST['poster']=$_FILES['poster']['name'];
-    move_uploaded_file($_FILES['poster']['tmp_name'],'../upload'.$_FILES['poster']['name']);
+    move_uploaded_file($_FILES['poster']['tmp_name'],'../upload/'.$_FILES['poster']['name']);
 }
 
 
 
-$_POST['onload']=$_POST['year']."-".$_POST['month']."-".$_POST['day'];
-unset($_POST['year'],$_POST['month'],$_POST['day']); // 刪除
+$_POST['ondate']=$_POST['year']."-".$_POST['month']."-".$_POST['day']; //$_POST的['onload'] 為 $_POST的年月日
+unset($_POST['year'],$_POST['month'],$_POST['day']); // 刪除年月日
 $_POST['sh']=1; // 預設都是顯示的
-$_POST['rank']=$Movie->math('max','id')+1;
+$_POST['rank']=$Movie->math('max','id')+1; // POST的[rank] 為 DB計算 最大 id +1
 
 $Movie->save($_POST);
 
-to("../back.php?do=movie");
+// to("../back.php?do=movie");
 
-// dd($_POST);
+dd($_POST);
+
 ?>
